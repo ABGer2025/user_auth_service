df7725529ac48ee66f118a128bf7ae21e1b2d299
ABGer2025 <abgabg2024@hotmail.com>
2025-07-18 18:57:05 +0200

    PLPG-API-20 Copy of data_manager created. Update of app.py with data/users.json completed.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
 from flask import Flask
+from data_manager import data_manager
+
+USERS_FILE = os.path.join(os.path.dirname(__file__), 'data', 'users.json')
+users = data_manager.read_data(USERS_FILE)
 
 app = Flask(__name__)
 
